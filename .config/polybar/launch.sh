11690d4dd79649bdb4fb8f136116a519bdc6bb47
Avinash Pandey <thehex360@gmail.com>
2020-11-07 02:35:51 +0530

    added autorandr postscript

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# stolen from https://github.com/polybar/polybar/issues/763#issuecomment-450940924
+(
+  flock 200
+
+  killall -q polybar
+
+  while pgrep -u $UID -x polybar > /dev/null; do sleep 0.5; done
+
+  outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
+  tray_output="eDP-1"
+
+  for m in $outputs; do
+    if [[ $m == "HDMI-2" ]]; then
+        tray_output=$m
+    fi
+  done
+
+  for m in $outputs; do
+    export MONITOR=$m
+    export TRAY_POSITION=none
+    if [[ $m == $tray_output ]]; then
+      TRAY_POSITION=right
+    fi
+
+    polybar --reload bar1 </dev/null >/var/tmp/polybar-$m.log 2>&1 200>&- &
+    disown
+  done
+) 200>/var/tmp/polybar-launch.lock
