53b1c453e1a97e623a235a9ecc32d8d352b963dd
Avinash Pandey <contact@mrpandey.com>
2022-11-01 16:48:48 +0530

    notion: install and uninstall scripts for notion using nativefier

---
+++ setup-scripts/nativefier-apps/notion/install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+this_script_dir=$( dirname -- "$0"; )
+installation_dir=~/.local/share
+icon_dir=~/.icons/hicolor/48x48/apps
+desktop_entry_dir=~/.local/share/applications
+
+# check if nativefier is installed
+if ! nativefier --version >/dev/null 2>&1
+then
+	echo "Nativefier not found. Exiting."
+	exit
+fi
+
+# nativefier will remove the directory notion-linux-x64 if it already exists
+nativefier --name notion --icon $this_script_dir/notion.png --background-color '#000000' https://www.notion.so/login $installation_dir
+rm -rf $installation_dir/notion
+mv $installation_dir/notion-linux-x64 $installation_dir/notion
+
+# copy desktop entry and icon
+mkdir -p $icon_dir $desktop_entry_dir
+cp $this_script_dir/notion.png $icon_dir/
+cp $this_script_dir/notion.desktop $desktop_entry_dir/
---
+++ setup-scripts/nativefier-apps/notion/uninstall.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+installation_dir=~/.local/share/notion
+icon=~/.icons/hicolor/48x48/apps/notion.png
+desktop_entry=~/.local/share/applications/notion.desktop
+
+rm -rf $installation_dir
+rm $icon
+rm $desktop_entry
